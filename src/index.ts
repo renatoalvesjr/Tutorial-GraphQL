e322b89bca1649132f6fd49a6a547ca99ca28d04
renatoalvesjr <renato.junior@estudante.iftm.edu.br>
2025-02-21 17:20:05 -0300

    mudança da porta por incompatibilidade

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ export const server = new ApolloServer({
   context,
 });
 
-const port = 3000;
-
+const port = 3200;
 server.listen({ port }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
