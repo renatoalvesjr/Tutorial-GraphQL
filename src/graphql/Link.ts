ec53b896f17daea7e9dd7abde137d980a50dadfb
renatoalvesjr <renato.junior@codiub.com.br>
2025-02-21 09:11:11 -0300

    filtragem por string no feed

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -22,7 +22,9 @@ export const Link = objectType({
             .findUnique({where: { id: parent.id}})
             .voters()
       }
-    })
+    });
+
+
   },
 });
 
@@ -31,8 +33,18 @@ export const LinkQuery = extendType({
   definition(t) {
     t.nonNull.list.nonNull.field("feed", {
       type: "Link",
+      args: {
+        filter: stringArg(),
+      },
       resolve(parent, args, context, info) {
-        return context.prisma.link.findMany();
+        const where = args.filter
+            ? {
+                OR: [
+                  {description: {contains: args.filter}},
+                  {url: {contains: args.filter}},
+                ]
+            } : {};
+        return context.prisma.link.findMany({where});
       },
     });
   },
